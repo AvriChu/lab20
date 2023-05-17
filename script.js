790add1e65453b8905f3828c2f3178257cedb223
AvriAvi <nastona367@gmail.com>
2023-05-18 00:13:14 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function img1(){
+    document.getElementById('imageChange1').src = 'img/image1.jpg';
+}
+function img2(){
+    document.getElementById('imageChange1').src = 'img/image2.jpg';
+}
+function img3(){
+    document.getElementById('imageChange1').src = 'img/image3.jpg';
+}
+function img4(){
+    document.getElementById('imageChange2').src = 'img/image4.jpg';
+}
+function img5(){
+    document.getElementById('imageChange2').src = 'img/image5.jpg';
+}
+function img6(){
+    document.getElementById('imageChange2').src = 'img/image6.jpg';
+}
